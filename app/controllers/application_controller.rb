36d7e9c50bf92b517672a53f126694b7a988b4e4
Kulgar <regmillet@ct2c.fr>
2020-07-02 23:09:35 +0200

    Added some translations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
 
+  before_action :set_locale
+
   class NotAuthorized < StandardError
   end
 
@@ -10,6 +12,15 @@ class ApplicationController < ActionController::Base
     def deny_access
       flash[:alert] = "Vous n'êtes pas administrateur"
       redirect_to root_path
+    end
+
+    def set_locale
+      # https://api.rubyonrails.org/
+      if params[:locale]
+        session[:locale] = params[:locale]
+        cookies.permanent[:persistent] = params[:locale]
+      end
+      I18n.locale = session[:locale]
     end
 
 end
