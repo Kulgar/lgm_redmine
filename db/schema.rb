e69849dbdf053ed02842ee2fd8edb1381d31a644
Kulgar <regmillet@ct2c.fr>
2020-07-02 21:25:30 +0200

    Added devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_02_184111) do
+ActiveRecord::Schema.define(version: 2020_07_02_191722) do
 
   create_table "admin_categories", force: :cascade do |t|
     t.string "name"
@@ -49,6 +49,19 @@ ActiveRecord::Schema.define(version: 2020_07_02_184111) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["project_id"], name: "index_tasks_on_project_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.boolean "admin", default: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
