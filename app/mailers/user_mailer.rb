4eb9e262d8027e031d07d203350c29ea21b79ac7
Kulgar <regmillet@ct2c.fr>
2020-07-02 22:31:42 +0200

    Simple attachment

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,8 @@ class UserMailer < ApplicationMailer
     @user = params[:user]
     @project = params[:project]
     @url  = project_url(@project)
+    # attachments['file.name']
+    attachments.inline['logo.png'] = File.read(Rails.root.join("app/assets/images/logo.png"))
     mail(to: @user.email, subject: "A new task was created for project #{@project.title}")
   end
 
