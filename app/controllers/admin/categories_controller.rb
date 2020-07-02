7821aa734535a244701f2e2fb41293181acc2a78
Kulgar <regmillet@ct2c.fr>
2020-07-02 20:40:47 +0200

    Went back to normal layout

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,4 @@
-class Admin::CategoriesController < AdminController
+class Admin::CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
 
   # GET /categories
